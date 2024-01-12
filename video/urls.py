2d5ef3bfce962aabb9c8f1abdcaa3815afffe7dc
hemap <hemaprabha.rct@gmail.com>
2024-01-12 21:40:31 +0530

    four commit

--- video/urls.py
+++ video/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('upload/', views.video_upload_view, name='video-upload-api'),
-    path('subtitles/', views.subtitles_view, name='subtitles-api'),
+    path('', views.subtitles_view, name='subtitles-api'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
